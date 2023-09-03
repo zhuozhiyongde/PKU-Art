eba4d8364a404aa7a8e9b983530dd444c38a10d2
Arthals <zhuozhiyongde@126.com>
2023-09-03 11:58:58 +0800

    update:v2.3.22

--- release/PKU-Art.user.js
+++ release/PKU-Art.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PKU-Art
 // @description  给你一个足够好看的教学网
-// @version      2.3.21
+// @version      2.3.22
 // @match        *://*.pku.edu.cn/*
 // @run-at       document-start
 // @author       Arthals
@@ -11,7 +11,7 @@
 // @namespace    https://github.com/zhuozhiyongde/PKU-Art
 // @supportURL   https://github.com/zhuozhiyongde/PKU-Art/issues
 // @updateURL    https://cdn.arthals.ink/release/PKU-Art.user.js
-// @date         2023/06/05
+// @date         2023/09/03
 // ==/UserScript==
 (function () {
     'use strict';
